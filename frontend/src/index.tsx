861c4c90de9f2796baf7d7d85fae1b30ef5cfdd3
Drew Budwin <dbudwin@foxguardsolutions.com>
2020-11-10 16:56:51 -0500

    ES6 module broke imports

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,8 +1,13 @@
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faCoffee } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
 import { Container, Heading, Hero } from "react-bulma-components";
 import ReactDOM from "react-dom";
 
 import registerServiceWorker from "./registerServiceWorker";
+
+library.add(faCoffee)
 
 const rootElement = document.querySelector("#root");
 
@@ -15,6 +20,7 @@ ReactDOM.render(
                 </Heading>
                 <Heading subtitle size={3}>
                     Subtitle
+                    <FontAwesomeIcon icon="coffee" />
                 </Heading>
             </Container>
         </Hero.Body>
